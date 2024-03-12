addc828b31708b5936e0eedffbbb3bd61329ec6a
Johannes Weiner <hannes@cmpxchg.org>
2024-03-12 11:34:12 -0400

    mm: zpool: return pool size in pages
    All zswap backends track their pool sizes in pages.  Currently they
    multiply by PAGE_SIZE for zswap, only for zswap to divide again in order
    to do limit math.  Report pages directly.
    
    Link: https://lkml.kernel.org/r/20240312153901.3441-2-hannes@cmpxchg.org
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Yosry Ahmed <yosryahmed@google.com>
    Reviewed-by: Chengming Zhou <chengming.zhou@linux.dev>
    Reviewed-by: Nhat Pham <nphamcs@gmail.com>
    Cc: Yosry Ahmed <yosryahmed@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -406,9 +406,9 @@ static void zs_zpool_unmap(void *pool, unsigned long handle)
 	zs_unmap_object(pool, handle);
 }
 
-static u64 zs_zpool_total_size(void *pool)
+static u64 zs_zpool_total_pages(void *pool)
 {
-	return zs_get_total_pages(pool) << PAGE_SHIFT;
+	return zs_get_total_pages(pool);
 }
 
 static struct zpool_driver zs_zpool_driver = {
@@ -421,7 +421,7 @@ static struct zpool_driver zs_zpool_driver = {
 	.free =			  zs_zpool_free,
 	.map =			  zs_zpool_map,
 	.unmap =		  zs_zpool_unmap,
-	.total_size =		  zs_zpool_total_size,
+	.total_pages =		  zs_zpool_total_pages,
 };
 
 MODULE_ALIAS("zpool-zsmalloc");
