b8040b710a1f9324a00aedad1efbb95458751782
Chao Yu <chao@kernel.org>
2025-04-16 11:56:56 +0800

    FROMGIT: f2fs: sysfs: export linear_lookup in features directory
    cat /sys/fs/f2fs/features/linear_lookup
    supported
    
    Bug: 410768629
    Bug: 425256403
    (cherry picked from commit 617e0491abe4d8d45c5110ca474c0feb428e6828
    https://git.kernel.org/pub/scm/linux/kernel/git/jaegeuk/f2fs.git dev)
    Link: https://lore.kernel.org/linux-f2fs-devel/20250416054805.1416834-2-chao@kernel.org
    Change-Id: I9762dc9d918f96e716a2e0b76a9fe6d168a6b56a
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Chao Yu <chao@kernel.org>
    (cherry picked from commit f9fc2b36bc40f22abf801422ff989da516109b8b)

--- fs/f2fs/sysfs.c
+++ fs/f2fs/sysfs.c
@@ -1149,6 +1149,9 @@ F2FS_FEATURE_RO_ATTR(readonly);
 F2FS_FEATURE_RO_ATTR(compression);
 #endif
 F2FS_FEATURE_RO_ATTR(pin_file);
+#ifdef CONFIG_UNICODE
+F2FS_FEATURE_RO_ATTR(linear_lookup);
+#endif
 
 #define ATTR_LIST(name) (&f2fs_attr_##name.attr)
 static struct attribute *f2fs_attrs[] = {
@@ -1296,6 +1299,9 @@ static struct attribute *f2fs_feat_attrs[] = {
 	ATTR_LIST(compression),
 #endif
 	ATTR_LIST(pin_file),
+#ifdef CONFIG_UNICODE
+	ATTR_LIST(linear_lookup),
+#endif
 	NULL,
 };
 ATTRIBUTE_GROUPS(f2fs_feat);
