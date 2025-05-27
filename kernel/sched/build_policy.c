1612cc1f42b025861c939e1cdfd093bfff97e4f0
Hu Lang <64276957+tryle17@users.noreply.github.com>
2025-05-28 02:44:12 +0800

    Update build_policy.c

--- kernel/sched/build_policy.c
+++ kernel/sched/build_policy.c
@@ -52,3 +52,11 @@
 #include "cputime.c"
 #include "deadline.c"
 
+#ifdef CONFIG_SCHED_CLASS_EXT
+# include "ext.c"
+# include "hmbird_sched_proc_main.c"
+#endif
+
+#ifdef CONFIG_SLIM_SCHED
+# include "slim_sysctl.c"
+#endif
