bad891aa9720f25b683cf7b180ab98c377eba512
brokestar233 <3765589194@qq.com>
2025-07-20 17:56:02 +0800

    printk: Force log buffer size to 16MB for non-early boot
    Increase the kernel log buffer size to 16MB during non-early boot by defining
    FORCED_LOG_BUF_LEN and setting new_log_buf_len accordingly. Additionally, ensure
    a minimum of 4096 descriptors by using max() in new_descs_count calculation to
    prevent insufficient descriptor allocation for larger buffers.
    
    This change enhances the kernel's ability to handle larger log outputs, improving
    debugging capabilities for systems with high logging demands.
    
    Signed-off-by: brokestar233 <3765589194@qq.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -18,6 +18,7 @@
  */
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+#define FORCED_LOG_BUF_LEN (16 * 1024 * 1024)
 
 #include <linux/kernel.h>
 #include <linux/mm.h>
@@ -1154,6 +1155,10 @@ void __init setup_log_buf(int early)
 	unsigned int free;
 	u64 seq;
 
+    if (!early) {
+        new_log_buf_len = FORCED_LOG_BUF_LEN;
+    }
+
 	/*
 	 * Some archs call setup_log_buf() multiple times - first is very
 	 * early, e.g. from setup_arch(), and second - when percpu_areas
@@ -1176,7 +1181,7 @@ void __init setup_log_buf(int early)
 		return;
 	}
 
-	new_descs_count = new_log_buf_len >> PRB_AVGBITS;
+	new_descs_count = max((FORCED_LOG_BUF_LEN >> PRB_AVGBITS), 4096U);
 	if (new_descs_count == 0) {
 		pr_err("new_log_buf_len: %lu too small\n", new_log_buf_len);
 		goto out;
