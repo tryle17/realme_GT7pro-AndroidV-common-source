szh0721/android_kernel_common_oneplus_sm8750

ANDROID: gki_defconfig: Add support for Oryon-1 CPU in ARM64 GKI configuration
Enable CONFIG_ORYON_1 in the ARM64 GKI defconfig to support the Oryon-1
CPU. Add corresponding -mcpu=orion-1 flag to KBUILD_CFLAGS in the
Makefile when CONFIG_ORYON_1 is enabled. This change ensures proper
compiler optimization for the Oryon-1 architecture.

Signed-off-by: brokestar233 <3765589194@qq.com>

Makefile
@@ -852,6 +852,10 @@ endif
 KBUILD_RUSTFLAGS += -Cdebug-assertions=$(if $(CONFIG_RUST_DEBUG_ASSERTIONS),y,n)
 KBUILD_RUSTFLAGS += -Coverflow-checks=$(if $(CONFIG_RUST_OVERFLOW_CHECKS),y,n)
 
+ifdef CONFIG_ORYON_1
+KBUILD_CFLAGS += -mcpu=orion-1
+endif
+
 ifdef CONFIG_LLVM_POLLY
 KBUILD_CFLAGS	+= -fvectorize -funroll-loops -mllvm -polly \
 		   -mllvm -polly-run-inliner \

tryle17/realme_GT7pro-AndroidV-common-source

Makefile
@@ -852,6 +852,10 @@ endif
 KBUILD_RUSTFLAGS += -Cdebug-assertions=$(if $(CONFIG_RUST_DEBUG_ASSERTIONS),y,n)
 KBUILD_RUSTFLAGS += -Coverflow-checks=$(if $(CONFIG_RUST_OVERFLOW_CHECKS),y,n)
 
+ifdef CONFIG_ARCH_ORYON
+KBUILD_CFLAGS += -mcpu=oryon-1
+endif
+
 ifdef CONFIG_LLVM_POLLY
 KBUILD_CFLAGS	+= -fvectorize -funroll-loops -mllvm -polly \
 		   -mllvm -polly-run-inliner \