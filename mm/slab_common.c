ee81dd5e14716d08a52e45086e941a5b69e60f85
tryle17 <langhuoshao@gmail.com>
2025-08-31 22:25:47 +0800

    Revert official changes

--- mm/slab_common.c
+++ mm/slab_common.c
@@ -728,6 +728,10 @@ struct kmem_cache *kmalloc_slab(size_t size, gfp_t flags, unsigned long caller)
 	if (s)
 		return s;
 
+	trace_android_vh_kmalloc_slab(index, flags, &s);
+	if (s)
+		return s;
+
 	return kmalloc_caches[kmalloc_type(flags, caller)][index];
 }
 
